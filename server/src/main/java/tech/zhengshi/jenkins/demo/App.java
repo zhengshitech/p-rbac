5f51a0cd43ef7a915547efcbbf8674567c978aa0
z <z@zhengshi.tech>
2018-09-26 21:55:48 +0800

    init project

---
+++ server/src/main/java/tech/zhengshi/jenkins/demo/App.java
@@ -0,0 +1,23 @@
+package tech.zhengshi.jenkins.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @RequestMapping(value = "works", method = RequestMethod.GET)
+    public String works() {
+        return "works";
+    }
+
+}
